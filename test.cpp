ed721298744bb4cc1f63f37d872a71b4ca74185c
TALONPA <pablobenito082@gmail.com>
2025-01-28 10:50:36 +0100

    TALONPA commit and add files

--- test.cpp
+++ test.cpp
@@ -23,6 +23,7 @@ void afficherRepere(sprite obj, int largeur, int hauteur, char grille[15][35]) {
                 // on affiche pacman en jaune
                 std::cout << "\033[33m" << obj.symbol << "\033[0m";
             }
+            // Tout ce qui suit dans cette fonction c'est la modelisation de la carte donc le placemant des murs
             else if(i == 0 || i == 14 || j == 0 || j == 34)
             {
                 grille[i][j] = 35;
@@ -38,32 +39,32 @@ void afficherRepere(sprite obj, int largeur, int hauteur, char grille[15][35]) {
                 grille[i][j] = 35;
                 cout << mur;
             }
-            else if(i == 2 && (j == 10 || j == 11 || j == 12 || j == 13 || j == 14 || j == 15 || j == 16 || j == 18 || j == 19 || j == 20 || j == 21 || j == 22 || j == 23 || j == 24 || j == 25))
+            else if((i == 2 || i == 12) && (j == 3 || j == 4 || j == 5 || j == 6 || j == 8 || j == 10 || j == 11 || j == 12 || j == 13 || j == 14 || j == 15 || j == 16 || j == 17 || j == 18 || j == 19 || j == 20 || j == 21 || j == 22 || j == 23 || j == 25 || j == 27 || j == 28 || j == 29 || j == 30 || j == 31))
             {
                 grille[i][j] = 35;
                 cout << mur;
             }
-            else if(i == 12 && (j == 10 || j == 11 || j == 12 || j == 13 || j == 14 || j == 15 || j == 16 || j == 18 || j == 19 || j == 20 || j == 21 || j == 22 || j == 23 || j == 24 || j == 25))
+            else if((j == 4 || j == 5 || j == 29 || j == 30) && (i == 4 || i == 5 || i == 6 || i == 7 || i == 8 || i == 9 || i == 10))
             {
                 grille[i][j] = 35;
                 cout << mur;
             }
-            else if((i == 5 || i == 9) && (j == 10 || j == 11 || j == 12 || j == 13 || j == 14 || j == 15 || j == 19 || j == 20 || j == 21 || j == 22 || j == 23 || j == 24 || j == 25))
+            else if((j == 7 || j == 8 || j == 25 || j == 26 || j == 27) && (i == 4 || i == 6 || i == 8 || i == 10))
             {
                 grille[i][j] = 35;
                 cout << mur;
             }
-            else if((j == 9 || j == 26) && (i == 5 || i == 6 || i == 7 || i == 8 || i == 9))
+            else if((j == 10 || j == 23) && (i == 6 || i == 7 || i == 8))
             {
                 grille[i][j] = 35;
                 cout << mur;
             }
-            else if(j == 6 && (i == 2 || i == 3 || i == 5 || i == 6 || i == 8 || i == 9 || i == 10 || i == 12))
+            else if((i == 4 || i == 10) && (j == 10 || j == 11 || j == 12 || j == 13 || j == 14 || j == 15 || j == 16 || j == 17 || j == 18 || j == 19 || j == 20 || j == 21 || j == 22 || j == 23))
             {
                 grille[i][j] = 35;
                 cout << mur;
             }
-            else if(j == 29 && (i == 2 || i == 3 || i == 5 || i == 6 || i == 8 || i == 9 || i == 10 || i == 12))
+            else if((i == 6) && (j == 11 || j == 12 || j == 13 || j == 14 || j == 19 || j == 20 || j == 21 || j == 22 || j == 23))
             {
                 grille[i][j] = 35;
                 cout << mur;
